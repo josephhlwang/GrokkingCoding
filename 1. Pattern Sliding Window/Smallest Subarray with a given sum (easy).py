f22b70626851e1b6d5d795b467c6cf19781ff177
josephhlwang <joseph001126@gmail.com>
2022-01-21 16:32:11 -0500

    Implemented solution.

--- 1. Pattern Sliding Window/Smallest Subarray with a given sum (easy).py
+++ 1. Pattern Sliding Window/Smallest Subarray with a given sum (easy).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
 
@@ -19,60 +19,62 @@ Example 3:
 Input: [3, 4, 1, 1, 6], S=8 
 Output: 3
 Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
-'''
+"""
 
-#mycode
-import math
-def smallest_subarray_with_given_sum(s, arr):
-  window_sum = 0
-  min_length = math.inf
-  window_start = 0
-
-  for window_end in range(0, len(arr)):
-    window_sum += arr[window_end]  # add the next element
-    # shrink the window as small as possible until the 'window_sum' is smaller than 's'
-    while window_sum >= s:
-      min_length = min(min_length, window_end - window_start + 1)
-      window_sum -= arr[window_start]
-      window_start += 1
-  if min_length == math.inf:
-    return 0
-  return min_length
-
-
-#answer
+# mycode
 import math
 
-def smallest_subarray_with_given_sum(s, arr):
-  window_sum = 0
-  min_length = math.inf
-  window_start = 0
 
-  for window_end in range(0, len(arr)):
-    window_sum += arr[window_end]  # add the next element
-    # shrink the window as small as possible until the 'window_sum' is smaller than 's'
-    while window_sum >= s:
-      min_length = min(min_length, window_end - window_start + 1)
-      window_sum -= arr[window_start]
-      window_start += 1
-  if min_length == math.inf:
-    return 0
-  return min_length
+def ans(targ, arr):
+    cur_sum = left = 0
+    min_l = float("inf")
+
+    for right in range(len(arr)):
+        cur_sum += arr[right]
+
+        while cur_sum >= targ:
+            min_l = min(min_l, right - left + 1)
+            cur_sum -= arr[left]
+            left += 1
+
+    return min_l
+
+
+# answer
+import math
+
+
+def smallest_subarray_with_given_sum(s, arr):
+    window_sum = 0
+    min_length = math.inf
+    window_start = 0
+
+    for window_end in range(0, len(arr)):
+        window_sum += arr[window_end]  # add the next element
+        # shrink the window as small as possible until the 'window_sum' is smaller than 's'
+        while window_sum >= s:
+            min_length = min(min_length, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+    if min_length == math.inf:
+        return 0
+    return min_length
 
 
 def main():
-  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])))
-  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])))
-  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])))
+    print("Smallest subarray length: " + str(ans(7, [2, 1, 5, 2, 3, 2])))
+    print("Smallest subarray length: " + str(ans(7, [2, 1, 5, 2, 8])))
+    print("Smallest subarray length: " + str(ans(8, [3, 4, 1, 1, 6])))
+
 
 main()
 
 
-'''
+"""
 Time Complexity 
 The time complexity of the above algorithm will be O(N). 
 The outer for loop runs for all elements and the inner while loop processes each element only once, therefore the time complexity of the algorithm will be O(N+N) which is asymptotically equivalent to O(N).
 
 Space Complexity 
 The algorithm runs in constant space O(1).
-'''
+"""
