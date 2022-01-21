eb6e64b6d7b5a53d504b2f605fcb28d6366901d1
josephhlwang <joseph001126@gmail.com>
2022-01-21 00:14:20 -0500

    Implemented solution.

--- 1. Pattern Sliding Window/Fruits into Baskets (medium).py
+++ 1. Pattern Sliding Window/Fruits into Baskets (medium).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 Given an array of characters where each character represents a fruit tree, you are given two baskets and your goal is to put maximum number of fruits in each basket. The only restriction is that each basket can have only one type of fruit.
 
@@ -18,66 +18,87 @@ Input: Fruit=['A', 'B', 'C', 'B', 'B', 'C']
 Output: 5
 Explanation: We can put 3 'B' in one basket and two 'C' in the other basket. 
 This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
-'''
+"""
 
-#mycode
+
+def ans(fruits):
+    left = max_len = 0
+
+    fruits_count = {}
+    for right in range(len(fruits)):
+        right_fruit = fruits[right]
+
+        if right_fruit not in fruits_count:
+            fruits_count[right_fruit] = 0
+        fruits_count[right_fruit] += 1
+
+        while len(fruits_count) > 2:
+            left_fruit = fruits[left]
+            fruits_count[left_fruit] -= 1
+            if fruits_count[left_fruit] == 0:
+                fruits_count.pop(left_fruit)
+            left += 1
+
+        max_len = max(right - left + 1, max_len)
+
+    return max_len
+
+
+# another sol
 def fruits_into_baskets(fruits):
-  max_len, win_start = 0, 0
-  dict_fruits={}
+    two_trees = {}
+    start_index = max_so_far = 0
+    for i in range(len(fruits)):
+        tree = fruits[i]
+        if tree not in two_trees:
+            two_trees[tree] = 0
+        two_trees[tree] += 1
 
-  for win_end in range(len(fruits)):
-    if fruits[win_end] not in dict_fruits:
-      dict_fruits[fruits[win_end]] = 1
-    else: 
-      dict_fruits[fruits[win_end]] += 1
-    
-    while len(dict_fruits) >2:
-      if dict_fruits[fruits[win_start]] == 1:
-        del dict_fruits[fruits[win_start]]
-      else:
-        dict_fruits[fruits[win_start]] -= 1
-      win_start += 1 
+        while len(two_trees) > 2:
+            start_fruit = fruits[start_index]
+            two_trees[start_fruit] -= 1
+            if two_trees[start_fruit] == 0:
+                two_trees.pop(start_fruit)
+            start_index += 1
 
-    if len(dict_fruits) == 2:
-      max_len=max(max_len, sum(dict_fruits.values()))
-      
-  return max_len
+        max_so_far = max(max_so_far, 1 + i - start_index)
+
+    return max_so_far
 
 
-#answer
-def fruits_into_baskets(fruits):
-  window_start = 0
-  max_length = 0
-  fruit_frequency = {}
+# answer
+def fruits_into_baskets_ans(fruits):
+    window_start = 0
+    max_length = 0
+    fruit_frequency = {}
 
-  # try to extend the range [window_start, window_end]
-  for window_end in range(len(fruits)):
-    right_fruit = fruits[window_end]
-    if right_fruit not in fruit_frequency:
-      fruit_frequency[right_fruit] = 0
-    fruit_frequency[right_fruit] += 1
+    # try to extend the range [window_start, window_end]
+    for window_end in range(len(fruits)):
+        right_fruit = fruits[window_end]
+        if right_fruit not in fruit_frequency:
+            fruit_frequency[right_fruit] = 0
+        fruit_frequency[right_fruit] += 1
 
-    # shrink the sliding window, until we are left with '2' fruits in the fruit frequency dictionary
-    while len(fruit_frequency) > 2:
-      left_fruit = fruits[window_start]
-      fruit_frequency[left_fruit] -= 1
-      if fruit_frequency[left_fruit] == 0:
-        del fruit_frequency[left_fruit]
-      window_start += 1  # shrink the window
-    max_length = max(max_length, window_end-window_start + 1)
-  return max_length
+        # shrink the sliding window, until we are left with '2' fruits in the fruit frequency dictionary
+        while len(fruit_frequency) > 2:
+            left_fruit = fruits[window_start]
+            fruit_frequency[left_fruit] -= 1
+            if fruit_frequency[left_fruit] == 0:
+                del fruit_frequency[left_fruit]
+            window_start += 1  # shrink the window
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
 
 
 def main():
-  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'A', 'C'])))
-  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C'])))
+    print("Maximum number of fruits: " + str(ans(["A", "B", "C", "A", "C", "B", "B"])))
+    print("Maximum number of fruits: " + str(ans(["A", "B", "C", "B", "B", "C"])))
 
 
 main()
 
 
-
-'''
+"""
 Time Complexity 
 The time complexity of the above algorithm will be O(N) where ‘N’ is the number of characters in the input array. 
 The outer for loop runs for all characters and the inner while loop processes each character only once, therefore the time complexity of the algorithm will be O(N+N)which is asymptotically equivalent to O(N).
@@ -91,5 +112,4 @@ Problem 1: Longest Substring with at most 2 distinct characters
 Given a string, find the length of the longest substring in it with at most two distinct characters.
 
 Solution: This problem is exactly similar to our parent problem.
-'''
-
+"""
--- 1. Pattern Sliding Window/Longest Substring with K Distinct Characters (medium).py
+++ 1. Pattern Sliding Window/Longest Substring with K Distinct Characters (medium).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement #
 Given a string, find the length of the longest substring in it with no more than K distinct characters.
 
@@ -19,71 +19,105 @@ Example 3:
 Input: String="cbbebi", K=3
 Output: 5
 Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
-'''
+"""
 
-#mycode
+# mycode
+def ans(str, k):
+    left = max_len = 0
+
+    char_count = {}
+    for right in range(len(str)):
+        right_char = str[right]
+
+        if right_char not in char_count:
+            char_count[right_char] = 0
+        char_count[right_char] += 1
+
+        while len(char_count) > k:
+            left_char = str[left]
+            char_count[left_char] -= 1
+            if char_count[left_char] == 0:
+                char_count.pop(left_char)
+            left += 1
+
+        max_len = max(right - left + 1, max_len)
+
+    return max_len
+
+
+# another sol
+
+
+def ans2(str, k):
+    left_index = longest_so_far = 0
+
+    char_count = {}
+    for right_index in range(len(str)):
+        right_char = str[right_index]
+        if right_char not in char_count:
+            char_count[right_char] = 0
+        char_count[right_char] += 1
+
+        while len(char_count) > k:
+            left_char = str[left_index]
+            char_count[left_char] -= 1
+            if char_count[left_char] == 0:
+                char_count.pop(left_char)
+            left_index += 1
+
+        longest_so_far = max(longest_so_far, 1 + right_index - left_index)
+
+    return longest_so_far
+
+
+# answer
 def longest_substring_with_k_distinct(str, k):
-  dict_arr={}
-  max_len, win_start = 0, 0
+    window_start = 0
+    max_length = 0
+    char_frequency = {}
 
-  for win_end in range(len(str)):
-    if str[win_end] not in dict_arr:
-      dict_arr[str[win_end]]=1
-    else:
-      dict_arr[str[win_end]] +=1
+    # in the following loop we'll try to extend the range [window_start, window_end]
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+        if right_char not in char_frequency:
+            char_frequency[right_char] = 0
+        char_frequency[right_char] += 1
 
-    while len(dict_arr) > k:
-      if dict_arr[str[win_start]] == 1:
-        del dict_arr[str[win_start]]
-      else: 
-        dict_arr[str[win_start]] -= 1
-      win_start += 1
-
-    if len(dict_arr) == k:
-      max_len=max(max_len,sum(dict_arr.values()))
-  
-  return max_len
-
-
-#answer
-def longest_substring_with_k_distinct(str, k):
-  window_start = 0
-  max_length = 0
-  char_frequency = {}
-
-  # in the following loop we'll try to extend the range [window_start, window_end]
-  for window_end in range(len(str)):
-    right_char = str[window_end]
-    if right_char not in char_frequency:
-      char_frequency[right_char] = 0
-    char_frequency[right_char] += 1
-
-    # shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
-    while len(char_frequency) > k:
-      left_char = str[window_start]
-      char_frequency[left_char] -= 1
-      if char_frequency[left_char] == 0:
-        del char_frequency[left_char]
-      window_start += 1  # shrink the window
-    # remember the maximum length so far
-    max_length = max(max_length, window_end-window_start + 1)
-  return max_length
+        # shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
+        while len(char_frequency) > k:
+            left_char = str[window_start]
+            char_frequency[left_char] -= 1
+            if char_frequency[left_char] == 0:
+                del char_frequency[left_char]
+            window_start += 1  # shrink the window
+        # remember the maximum length so far
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
 
 
 def main():
-  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 2)))
-  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 1)))
-  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 3)))
+    print(
+        "Length of the longest substring: "
+        + str(longest_substring_with_k_distinct("araaci", 2))
+    )
+    print(
+        "Length of the longest substring: "
+        + str(longest_substring_with_k_distinct("araaci", 1))
+    )
+    print(
+        "Length of the longest substring: "
+        + str(longest_substring_with_k_distinct("cbbebi", 3))
+    )
 
 
 main()
 
 
-'''
+"""
 Time Complexity 
 The time complexity of the above algorithm will be O(N) where ‘N’ is the number of characters in the input string. 
 The outer for loop runs for all characters and the inner while loop processes each character only once, therefore the time complexity of the algorithm will be O(N+N) which is asymptotically equivalent to O(N).
 
 Space Complexity 
 The space complexity of the algorithm is O(K), as we will be storing a maximum of ‘K+1’ characters in the HashMap.
-'''
+"""
