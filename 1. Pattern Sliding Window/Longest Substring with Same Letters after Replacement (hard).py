b01d92d6c49bb4a59b155c05e9accef8dc354171
josephhlwang <joseph001126@gmail.com>
2022-01-23 23:04:06 -0500

    Implemented solution.

--- 1. Pattern Sliding Window/Longest Substring with Same Letters after Replacement (hard).py
+++ 1. Pattern Sliding Window/Longest Substring with Same Letters after Replacement (hard).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter, find the length of the longest substring having the same letters after replacement.
 
@@ -19,69 +19,94 @@ Example 3:
 Input: String="abccde", k=1
 Output: 3
 Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
-'''
+"""
 
-#mycode
+
+def ans(str, k):
+    left = maj = max_l = 0
+
+    char_freq = {}
+    for right in range(len(str)):
+        right_char = str[right]
+        if right_char not in char_freq:
+            char_freq[right_char] = 0
+        char_freq[right_char] += 1
+
+        maj = max(maj, char_freq[right_char])
+        while right - left + 1 - maj > k:
+            left_char = str[left]
+            char_freq[left_char] -= 1
+            maj = max(maj, char_freq[left_char])
+            if char_freq[left_char] == 0:
+                char_freq.pop(left_char)
+            left += 1
+
+        max_l = max(right - left + 1, max_l)
+
+    return max_l
+
+
+# mycode
 def length_of_longest_substring(str, k):
-  # TODO: Write your code here
-  win_start, max_len, cnt = 0, 0, 0
-  dict_str={}
-  
-  for win_end in range(len(str)):
-    if str[win_end] not in dict_str:
-      dict_str[str[win_end]] = 1
-    else: 
-      dict_str[str[win_end]] += 1
+    # TODO: Write your code here
+    win_start, max_len, cnt = 0, 0, 0
+    dict_str = {}
 
-    cnt=max(dict_str.values())
-    while win_end - win_start + 1 -cnt > k:
-      dict_str[str[win_start]] -= 1
-      win_start +=1
-    
-    max_len=max(max_len, win_end - win_start + 1)
+    for win_end in range(len(str)):
+        if str[win_end] not in dict_str:
+            dict_str[str[win_end]] = 1
+        else:
+            dict_str[str[win_end]] += 1
 
-  return max_len
+        cnt = max(dict_str.values())
+        while win_end - win_start + 1 - cnt > k:
+            dict_str[str[win_start]] -= 1
+            win_start += 1
+
+        max_len = max(max_len, win_end - win_start + 1)
+
+    return max_len
 
 
-#answer
+# answer
 def length_of_longest_substring(str, k):
-  window_start, max_length, max_repeat_letter_count = 0, 0, 0
-  frequency_map = {}
+    window_start, max_length, max_repeat_letter_count = 0, 0, 0
+    frequency_map = {}
 
-  # Try to extend the range [window_start, window_end]
-  for window_end in range(len(str)):
-    right_char = str[window_end]
-    if right_char not in frequency_map:
-      frequency_map[right_char] = 0
-    frequency_map[right_char] += 1
-    max_repeat_letter_count = max(
-      max_repeat_letter_count, frequency_map[right_char])
+    # Try to extend the range [window_start, window_end]
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+        if right_char not in frequency_map:
+            frequency_map[right_char] = 0
+        frequency_map[right_char] += 1
+        max_repeat_letter_count = max(
+            max_repeat_letter_count, frequency_map[right_char]
+        )
 
-    # Current window size is from window_start to window_end, overall we have a letter which is
-    # repeating 'max_repeat_letter_count' times, this means we can have a window which has one letter
-    # repeating 'max_repeat_letter_count' times and the remaining letters we should replace.
-    # if the remaining letters are more than 'k', it is the time to shrink the window as we
-    # are not allowed to replace more than 'k' letters
-    if (window_end - window_start + 1 - max_repeat_letter_count) > k:
-      left_char = str[window_start]
-      frequency_map[left_char] -= 1
-      window_start += 1
+        # Current window size is from window_start to window_end, overall we have a letter which is
+        # repeating 'max_repeat_letter_count' times, this means we can have a window which has one letter
+        # repeating 'max_repeat_letter_count' times and the remaining letters we should replace.
+        # if the remaining letters are more than 'k', it is the time to shrink the window as we
+        # are not allowed to replace more than 'k' letters
+        if (window_end - window_start + 1 - max_repeat_letter_count) > k:
+            left_char = str[window_start]
+            frequency_map[left_char] -= 1
+            window_start += 1
 
-    max_length = max(max_length, window_end - window_start + 1)
-  return max_length
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
 
 
 def main():
-  print(length_of_longest_substring("aabccbb", 2))
-  print(length_of_longest_substring("abbcb", 1))
-  print(length_of_longest_substring("abccde", 1))
+    print(ans("aabccbb", 2))
+    print(ans("abbcb", 1))
+    print(ans("abccde", 1))
 
 
 main()
 
 
-
-'''
+"""
 Time Complexity 
 The time complexity of the above algorithm will be O(N) where ‘N’ is the number of letters in the input string.
 
@@ -89,4 +114,4 @@ Space Complexity
 As we are expecting only the lower case letters in the input string, we can conclude that the space complexity will be O(26), to store each letter’s frequency in the HashMap, which is asymptotically equal to O(1).
 
 
-'''
+"""
