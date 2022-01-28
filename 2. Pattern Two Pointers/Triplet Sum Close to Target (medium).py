3c1ebcf1582c3d130719d9d9862c5f9bd75fba08
josephhlwang <joseph001126@gmail.com>
2022-01-27 20:16:19 -0500

    Implemented solutions.

--- 2. Pattern Two Pointers/Triplet Sum Close to Target (medium).py
+++ 2. Pattern Two Pointers/Triplet Sum Close to Target (medium).py
@@ -1,4 +1,4 @@
-'''
+"""
 Triplet Sum Close to Target (medium)
 
 Problem Statement 
@@ -22,75 +22,118 @@ Example 3:
 Input: [1, 0, 1, 1], target=100
 Output: 3
 Explanation: The triplet [1, 1, 1] has the closest sum to the target.
-'''
-
-#mycode
+"""
 import math
 
+
+def ans(arr, target):
+    result = [float("inf"), float("inf")]
+    for i in range(len(arr)):
+        third = arr[i]
+        two_sum_close(arr, i, result, third, target)
+        if result[0] == 0:
+            break
+
+    return result[1]
+
+
+def two_sum_close(arr, start, result, third, target):
+    min_dist = result[0]
+    min_sum = result[1]
+    left, right = start, len(arr) - 1
+
+    while left < right:
+        sum = arr[left] + arr[right]
+
+        if abs(target - (third + sum)) < min_dist and third + sum < min_sum:
+            result[0] = abs(target - (third + sum))
+            result[1] = third + sum
+
+        if result[0] == 0:
+            break
+        elif third + sum < target:
+            left += 1
+        else:
+            right -= 1
+
+
 def triplet_sum_close_to_target(arr, target_sum):
-  
-  # TODO: Write your code here
-  arr.sort()
-  min_sum, err_min = 0, math.inf
 
-  for i in range(len(arr)):
-    j, k = i+1, len(arr)-1
-    while j < k:
-      err = abs(arr[i]+arr[j]+arr[k]-target_sum)
-      if err < err_min:
-        err_min=err
-        min_sum = arr[i]+arr[j]+arr[k]
-      elif err == err_min:
-        min_sum=min(min_sum,arr[i]+arr[j]+arr[k])
+    # TODO: Write your code here
+    arr.sort()
+    min_sum, err_min = 0, math.inf
 
-      if arr[i]+arr[j]+arr[k] < target_sum:
-        j += 1
-      elif arr[i]+arr[j]+arr[k] > target_sum:
-        k -= 1
+    for i in range(len(arr)):
+        j, k = i + 1, len(arr) - 1
+        while j < k:
+            err = abs(arr[i] + arr[j] + arr[k] - target_sum)
+            if err < err_min:
+                err_min = err
+                min_sum = arr[i] + arr[j] + arr[k]
+            elif err == err_min:
+                min_sum = min(min_sum, arr[i] + arr[j] + arr[k])
 
-  return min_sum
+            if arr[i] + arr[j] + arr[k] < target_sum:
+                j += 1
+            elif arr[i] + arr[j] + arr[k] > target_sum:
+                k -= 1
+
+    return min_sum
 
 
-
-#answer
+# answer
 import math
+
+
 def triplet_sum_close_to_target(arr, target_sum):
-  arr.sort()
-  smallest_difference = math.inf
-  for i in range(len(arr)-2):
-    left = i + 1
-    right = len(arr) - 1
-    while (left < right):
-      target_diff = target_sum - arr[i] - arr[left] - arr[right]
-      if target_diff == 0:  # we've found a triplet with an exact sum
-        return target_sum - target_diff  # return sum of all the numbers
+    arr.sort()
+    smallest_difference = math.inf
+    for i in range(len(arr) - 2):
+        left = i + 1
+        right = len(arr) - 1
+        while left < right:
+            target_diff = target_sum - arr[i] - arr[left] - arr[right]
+            if target_diff == 0:  # we've found a triplet with an exact sum
+                return target_sum - target_diff  # return sum of all the numbers
 
-      # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
-      if abs(target_diff) < abs(smallest_difference) or (abs(target_diff) == abs(smallest_difference) and target_diff > smallest_difference):
-        smallest_difference = target_diff  # save the closest and the biggest difference
+            # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
+            if abs(target_diff) < abs(smallest_difference) or (
+                abs(target_diff) == abs(smallest_difference)
+                and target_diff > smallest_difference
+            ):
+                smallest_difference = (
+                    target_diff  # save the closest and the biggest difference
+                )
 
-      if target_diff > 0:
-        left += 1  # we need a triplet with a bigger sum
-      else:
-        right -= 1  # we need a triplet with a smaller sum
+            if target_diff > 0:
+                left += 1  # we need a triplet with a bigger sum
+            else:
+                right -= 1  # we need a triplet with a smaller sum
 
-  return target_sum - smallest_difference
+    return target_sum - smallest_difference
 
 
 def main():
-  print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
-  print(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
-  print(triplet_sum_close_to_target([1, 0, 1, 1], 100))
+    print(ans([-2, 0, 1, 2], 2))
+    print(ans([-3, -1, 1, 2], 1))
+    print(ans([1, 0, 1, 1], 100))
+    print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
+    print(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
+    print(triplet_sum_close_to_target([1, 0, 1, 1], 100))
+
+    # print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
+    # print(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
+    # print(triplet_sum_close_to_target([1, 0, 1, 1], 100))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 Sorting the array will take O(N* logN)O(N∗logN). Overall searchTriplet() will take O(N * logN + N^2), 
 which is asymptotically equivalent to O(N^2).
 
 Space complexity 
 The space complexity of the above algorithm will be O(N) which is required for sorting.
-'''
+"""
--- 2. Pattern Two Pointers/Triplet Sum to Zero (medium).py
+++ 2. Pattern Two Pointers/Triplet Sum to Zero (medium).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
 
@@ -13,80 +13,119 @@ Example 2:
 Input: [-5, 2, -1, -2, 3]
 Output: [[-5, 2, 3], [-2, -1, 3]]
 Explanation: There are two unique triplets whose sum is equal to zero.
-'''
+"""
 
 
-#mycode 
+from email import iterators
+
+
+def ans(arr):
+    arr.sort()
+    results = []
+    for i in range(len(arr)):
+        target = arr[i]
+        results.extend(two_sum(arr, i + 1, target))
+
+    return results
+
+
+def two_sum(arr, start, target):
+    left, right = start, len(arr) - 1
+    results = []
+    while left < right:
+        sum = arr[left] + arr[right]
+
+        if target + sum == 0:
+            results.append([target, arr[left], arr[right]])
+            left += 1
+            right -= 1
+            while left < right and arr[left] == arr[left - 1]:
+                left += 1
+            while left < right and arr[right] == arr[right + 1]:
+                right -= 1
+        elif target + sum > 0:
+            right -= 1
+        else:
+            left += 1
+
+    return results
+
+
+# mycode
 def search_triplets(arr):
-  triplets = []
-  arr.sort()
-  # TODO: Write your code here
-  for i in range(len(arr)):
-    if i > 0 and arr[i] == arr[i-1]:
-      continue
-    search_pair(arr, -arr[i], i+1, triplets)
+    triplets = []
+    arr.sort()
+    # TODO: Write your code here
+    for i in range(len(arr)):
+        if i > 0 and arr[i] == arr[i - 1]:
+            continue
+        search_pair(arr, -arr[i], i + 1, triplets)
 
-  return triplets
+    return triplets
+
 
 def search_pair(arr, target_sum, left, triplets):
-  right = len(arr)-1
-  while left < right:
-    if arr[left] + arr[right] == target_sum:
-      triplets.append([-target_sum, arr[left], arr[right]])
-      left += 1
-      right -= 1
+    right = len(arr) - 1
+    while left < right:
+        if arr[left] + arr[right] == target_sum:
+            triplets.append([-target_sum, arr[left], arr[right]])
+            left += 1
+            right -= 1
 
-      while left < right and arr[left] == arr[left-1]:
-        left += 1
-      while left < right and arr[right] == arr[right+1]:
-        right -= 1
+            while left < right and arr[left] == arr[left - 1]:
+                left += 1
+            while left < right and arr[right] == arr[right + 1]:
+                right -= 1
 
-    elif arr[left] + arr[right] > target_sum:
-      right -=1
-    else:
-      left += 1
+        elif arr[left] + arr[right] > target_sum:
+            right -= 1
+        else:
+            left += 1
 
 
-
-#answer
+# answer
 def search_triplets(arr):
-  arr.sort()
-  triplets = []
-  for i in range(len(arr)):
-    if i > 0 and arr[i] == arr[i-1]:  # skip same element to avoid duplicate triplets
-      continue
-    search_pair(arr, -arr[i], i+1, triplets)
+    arr.sort()
+    triplets = []
+    for i in range(len(arr)):
+        if (
+            i > 0 and arr[i] == arr[i - 1]
+        ):  # skip same element to avoid duplicate triplets
+            continue
+        search_pair(arr, -arr[i], i + 1, triplets)
 
-  return triplets
+    return triplets
 
 
 def search_pair(arr, target_sum, left, triplets):
-  right = len(arr) - 1
-  while(left < right):
-    current_sum = arr[left] + arr[right]
-    if current_sum == target_sum:  # found the triplet
-      triplets.append([-target_sum, arr[left], arr[right]])
-      left += 1
-      right -= 1
-      while left < right and arr[left] == arr[left - 1]:
-        left += 1  # skip same element to avoid duplicate triplets
-      while left < right and arr[right] == arr[right + 1]:
-        right -= 1  # skip same element to avoid duplicate triplets
-    elif target_sum > current_sum:
-      left += 1  # we need a pair with a bigger sum
-    else:
-      right -= 1  # we need a pair with a smaller sum
+    right = len(arr) - 1
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        if current_sum == target_sum:  # found the triplet
+            triplets.append([-target_sum, arr[left], arr[right]])
+            left += 1
+            right -= 1
+            while left < right and arr[left] == arr[left - 1]:
+                left += 1  # skip same element to avoid duplicate triplets
+            while left < right and arr[right] == arr[right + 1]:
+                right -= 1  # skip same element to avoid duplicate triplets
+        elif target_sum > current_sum:
+            left += 1  # we need a pair with a bigger sum
+        else:
+            right -= 1  # we need a pair with a smaller sum
 
 
 def main():
-  print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
-  print(search_triplets([-5, 2, -1, -2, 3]))
+    print(ans([-3, 0, 1, 2, -1, 1, -2]))
+    print(ans([-5, 2, -1, -2, 3]))
+    print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
+    print(search_triplets([-5, 2, -1, -2, 3]))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 Sorting the array will take O(N * logN). 
 The searchPair() function will take O(N). 
@@ -96,4 +135,4 @@ this means that overall searchTriplets() will take O(N * logN + N^2), which is a
 Space complexity 
 Ignoring the space required for the output array, 
 the space complexity of the above algorithm will be O(N) which is required for sorting.
-'''
+"""
