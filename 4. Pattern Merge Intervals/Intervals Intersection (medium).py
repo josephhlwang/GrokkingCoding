220c248d58cc4c5fe39035e0a930fc873522228c
josephhlwang <joseph001126@gmail.com>
2022-02-14 08:28:08 -0500

    Implemented solution.

--- 4. Pattern Merge Intervals/Intervals Intersection (medium).py
+++ 4. Pattern Merge Intervals/Intervals Intersection (medium).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
 
@@ -13,120 +13,173 @@ Example 2:
 Input: arr1=[[1, 3], [5, 7], [9, 12]], arr2=[[5, 10]]
 Output: [5, 7], [9, 10]
 Explanation: The output list contains the common intervals between the two lists.
-'''
+"""
 
 
-#mycode
+from code import interact
+
+
+def ans(intervals_a, intervals_b):
+
+    i = 0
+    j = 0
+    start, end = 0, 1
+    results = []
+
+    while i < len(intervals_a) and j < len(intervals_b):
+        if (
+            intervals_b[j][start] <= intervals_a[i][start] <= intervals_b[j][end]
+            or intervals_b[j][start] <= intervals_a[i][end] <= intervals_b[j][end]
+        ):
+            results.append(
+                [
+                    max(intervals_b[j][start], intervals_a[i][start]),
+                    min(intervals_b[j][end], intervals_a[i][end]),
+                ]
+            )
+
+        if intervals_a[i][end] < intervals_b[j][end]:
+            i += 1
+        else:
+            j += 1
+
+    return results
+
+
+# mycode
 def merge(intervals_a, intervals_b):
-  result = []
+    result = []
 
-  # TODO: Write your code here
+    # TODO: Write your code here
 
-  i,j =0, 0 
-  while i < len(intervals_a) and j < len(intervals_b):
-    if intervals_a[i][1] < intervals_b[j][0]:
-      i+=1
-    elif intervals_b[j][1] < intervals_a[i][0]:
-      j+=1
-    else:
-      start=max(intervals_a[i][0], intervals_b[j][0])
-      end=min(intervals_a[i][1],intervals_b[j][1])
-      result.append([start,end])
-      if intervals_a[i][1] == end:
-        i += 1
-      if intervals_b[j][1] == end:
-        j += 1
+    i, j = 0, 0
+    while i < len(intervals_a) and j < len(intervals_b):
+        if intervals_a[i][1] < intervals_b[j][0]:
+            i += 1
+        elif intervals_b[j][1] < intervals_a[i][0]:
+            j += 1
+        else:
+            start = max(intervals_a[i][0], intervals_b[j][0])
+            end = min(intervals_a[i][1], intervals_b[j][1])
+            result.append([start, end])
+            if intervals_a[i][1] == end:
+                i += 1
+            if intervals_b[j][1] == end:
+                j += 1
 
-  return result
+    return result
 
 
 def main():
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]])))
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
+    print(
+        "Intervals Intersectionz: "
+        + str(ans([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]))
+    )
+    print("Intervals Intersectionz: " + str(ans([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
 
 
 main()
 
 
-#answer
+# answer
 def merge(intervals_a, intervals_b):
-  result = []
-  i, j, start, end = 0, 0, 0, 1
+    result = []
+    i, j, start, end = 0, 0, 0, 1
 
-  while i < len(intervals_a) and j < len(intervals_b):
-    # check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
-    a_overlaps_b = intervals_a[i][start] >= intervals_b[j][start] and \
-                   intervals_a[i][start] <= intervals_b[j][end]
+    while i < len(intervals_a) and j < len(intervals_b):
+        # check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
+        a_overlaps_b = (
+            intervals_a[i][start] >= intervals_b[j][start]
+            and intervals_a[i][start] <= intervals_b[j][end]
+        )
 
-    # check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
-    b_overlaps_a = intervals_b[j][start] >= intervals_a[i][start] and \
-                   intervals_b[j][start] <= intervals_a[i][end]
+        # check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
+        b_overlaps_a = (
+            intervals_b[j][start] >= intervals_a[i][start]
+            and intervals_b[j][start] <= intervals_a[i][end]
+        )
 
-    # store the the intersection part
-    if (a_overlaps_b or b_overlaps_a):
-      result.append([max(intervals_a[i][start], intervals_b[j][start]), min(
-        intervals_a[i][end], intervals_b[j][end])])
+        # store the the intersection part
+        if a_overlaps_b or b_overlaps_a:
+            result.append(
+                [
+                    max(intervals_a[i][start], intervals_b[j][start]),
+                    min(intervals_a[i][end], intervals_b[j][end]),
+                ]
+            )
 
-    # move next from the interval which is finishing first
-    if intervals_a[i][end] < intervals_b[j][end]:
-      i += 1
-    else:
-      j += 1
+        # move next from the interval which is finishing first
+        if intervals_a[i][end] < intervals_b[j][end]:
+            i += 1
+        else:
+            j += 1
 
-  return result
+    return result
 
 
 def main():
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]])))
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
+    print(
+        "Intervals Intersection: "
+        + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]))
+    )
+    print("Intervals Intersection: " + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
 
 
 main()
 
 
-
-#answer
+# answer
 def merge(intervals_a, intervals_b):
-  result = []
-  i, j, start, end = 0, 0, 0, 1
+    result = []
+    i, j, start, end = 0, 0, 0, 1
 
-  while i < len(intervals_a) and j < len(intervals_b):
-    # check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
-    a_overlaps_b = intervals_a[i][start] >= intervals_b[j][start] and \
-                   intervals_a[i][start] <= intervals_b[j][end]
+    while i < len(intervals_a) and j < len(intervals_b):
+        # check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
+        a_overlaps_b = (
+            intervals_a[i][start] >= intervals_b[j][start]
+            and intervals_a[i][start] <= intervals_b[j][end]
+        )
 
-    # check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
-    b_overlaps_a = intervals_b[j][start] >= intervals_a[i][start] and \
-                   intervals_b[j][start] <= intervals_a[i][end]
+        # check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
+        b_overlaps_a = (
+            intervals_b[j][start] >= intervals_a[i][start]
+            and intervals_b[j][start] <= intervals_a[i][end]
+        )
 
-    # store the the intersection part
-    if (a_overlaps_b or b_overlaps_a):
-      result.append([max(intervals_a[i][start], intervals_b[j][start]), min(
-        intervals_a[i][end], intervals_b[j][end])])
+        # store the the intersection part
+        if a_overlaps_b or b_overlaps_a:
+            result.append(
+                [
+                    max(intervals_a[i][start], intervals_b[j][start]),
+                    min(intervals_a[i][end], intervals_b[j][end]),
+                ]
+            )
 
-    # move next from the interval which is finishing first
-    if intervals_a[i][end] < intervals_b[j][end]:
-      i += 1
-    else:
-      j += 1
+        # move next from the interval which is finishing first
+        if intervals_a[i][end] < intervals_b[j][end]:
+            i += 1
+        else:
+            j += 1
 
-  return result
+    return result
 
 
 def main():
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]])))
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
+    print(
+        "Intervals Intersection: "
+        + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]))
+    )
+    print("Intervals Intersection: " + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 As we are iterating through both the lists once, the time complexity of the above algorithm is O(N + M), 
 where ‘N’ and ‘M’ are the total number of intervals in the input arrays respectively.
 
 Space complexity 
 Ignoring the space needed for the result list, the algorithm runs in constant space O(1).
-'''
-
+"""
