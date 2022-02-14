8e7808e7dce813aef40da4477c73c71267e713d1
josephhlwang <joseph001126@gmail.com>
2022-02-14 07:25:14 -0500

    Implemented solution.

--- 4. Pattern Merge Intervals/Insert Interval (medium).py
+++ 4. Pattern Merge Intervals/Insert Interval (medium).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
 
@@ -19,84 +19,127 @@ Example 3:
 Input: Intervals=[[2,3],[5,7]], New Interval=[1,4]
 Output: [[1,4], [5,7]]
 Explanation: After insertion, since [1,4] overlaps with [2,3], we merged them into one [1,4].
-'''
+"""
 
 
-#mycode
+def ans(intervals, new_interval):
+    i = 0
+    start, end = 0, 1
+    results = []
+
+    while i < len(intervals) and intervals[i][start] < new_interval[start]:
+        i += 1
+    intervals.insert(i, new_interval)
+
+    # print(intervals)
+
+    cur_start = intervals[0][start]
+    cur_end = intervals[0][end]
+    i = 1
+    while i < len(intervals):
+        if intervals[i][start] <= cur_end:
+            cur_end = max(intervals[i][end], cur_end)
+        else:
+            results.append([cur_start, cur_end])
+            cur_start = intervals[i][start]
+            cur_end = intervals[i][end]
+        i += 1
+    results.append([cur_start, cur_end])
+    return results
+
+
+# mycode
 def insert(intervals, new_interval):
-  merged = []
-  # TODO: Write your code here
-  index=0
-  for i in range(len(intervals)):
-    
-    if intervals[i][1] < new_interval[0]:
-      merged.append(intervals[i])
-    
-    if intervals[i][0] <= new_interval[1] and intervals[i][1] >= new_interval[0]:
-      new_interval[0]=min(new_interval[0],intervals[i][0])
-      new_interval[1]=max(new_interval[1],intervals[i][1])
-      index = i
+    merged = []
+    # TODO: Write your code here
+    index = 0
+    for i in range(len(intervals)):
 
-    if intervals[i][0] > new_interval[1]:
-      if i==index+1:
+        if intervals[i][1] < new_interval[0]:
+            merged.append(intervals[i])
+
+        if intervals[i][0] <= new_interval[1] and intervals[i][1] >= new_interval[0]:
+            new_interval[0] = min(new_interval[0], intervals[i][0])
+            new_interval[1] = max(new_interval[1], intervals[i][1])
+            index = i
+
+        if intervals[i][0] > new_interval[1]:
+            if i == index + 1:
+                merged.append(new_interval)
+                new_interval = [-1, -1]
+            merged.append(intervals[i])
+    if new_interval != [-1, -1]:
         merged.append(new_interval)
-        new_interval=[-1,-1]
-      merged.append(intervals[i])
-  if new_interval != [-1,-1]:
-    merged.append(new_interval)
-  return merged
+    return merged
 
 
 def main():
-  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
-  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
-  print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
+    print(
+        "Intervals after inserting the new interval: "
+        + str(ans([[1, 3], [5, 7], [8, 12]], [4, 6]))
+    )
+    print(
+        "Intervals after inserting the new interval: "
+        + str(ans([[1, 3], [5, 7], [8, 12]], [4, 10]))
+    )
+    print(
+        "Intervals after inserting the new interval: "
+        + str(ans([[2, 3], [5, 7]], [1, 4]))
+    )
 
 
 main()
 
 
-
-#answer
+# answer
 def insert(intervals, new_interval):
-  merged = []
-  i, start, end = 0, 0, 1
+    merged = []
+    i, start, end = 0, 0, 1
 
-  # skip (and add to output) all intervals that come before the 'new_interval'
-  while i < len(intervals) and intervals[i][end] < new_interval[start]:
-    merged.append(intervals[i])
-    i += 1
+    # skip (and add to output) all intervals that come before the 'new_interval'
+    while i < len(intervals) and intervals[i][end] < new_interval[start]:
+        merged.append(intervals[i])
+        i += 1
 
-  # merge all intervals that overlap with 'new_interval'
-  while i < len(intervals) and intervals[i][start] <= new_interval[end]:
-    new_interval[start] = min(intervals[i][start], new_interval[start])
-    new_interval[end] = max(intervals[i][end], new_interval[end])
-    i += 1
+    # merge all intervals that overlap with 'new_interval'
+    while i < len(intervals) and intervals[i][start] <= new_interval[end]:
+        new_interval[start] = min(intervals[i][start], new_interval[start])
+        new_interval[end] = max(intervals[i][end], new_interval[end])
+        i += 1
 
-  # insert the new_interval
-  merged.append(new_interval)
+    # insert the new_interval
+    merged.append(new_interval)
 
-  # add all the remaining intervals to the output
-  while i < len(intervals):
-    merged.append(intervals[i])
-    i += 1
-  return merged
+    # add all the remaining intervals to the output
+    while i < len(intervals):
+        merged.append(intervals[i])
+        i += 1
+    return merged
 
 
 def main():
-  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
-  print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
-  print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
+    print(
+        "Intervals after inserting the new interval: "
+        + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6]))
+    )
+    print(
+        "Intervals after inserting the new interval: "
+        + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10]))
+    )
+    print(
+        "Intervals after inserting the new interval: "
+        + str(insert([[2, 3], [5, 7]], [1, 4]))
+    )
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 As we are iterating through all the intervals only once, the time complexity of the above algorithm is O(N)O, 
 where ‘N’ is the total number of intervals.
 
 Space complexity 
 The space complexity of the above algorithm will be O(N) as we need to return a list containing all the merged intervals.
-'''
+"""
