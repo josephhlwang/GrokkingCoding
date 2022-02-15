fc8553c317ba798f156cf7c411ad4cc1d7de9850
josephhlwang <joseph001126@gmail.com>
2022-02-15 11:34:32 -0500

    Implemented solution.

--- 4. Pattern Merge Intervals/Problem Challenge 1 - Minimum Meeting Rooms (hard) .py
+++ 4. Pattern Merge Intervals/Problem Challenge 1 - Minimum Meeting Rooms (hard) .py
@@ -1,4 +1,4 @@
-'''
+"""
 Solution Review: Problem Challenge 1 - Minimum Meeting Rooms (hard) 
 Given a list of intervals representing the start and end time of ‘N’ meetings, 
 find the minimum number of rooms required to hold all the meetings.
@@ -30,101 +30,140 @@ Output: 2
 Explanation: We will need one room for [2,3] and [3,5], and another room for [2,4] and [4,5].
  
 Here is a visual representation of Example 4:
-'''
+"""
 
-
-
-#mycode
 from heapq import *
 
 
+def ans(meetings):
+    meetings.sort(key=lambda x: x.start)
+    max_rooms = 0
+    cur_meetings = []
+
+    for meeting in meetings:
+        while cur_meetings and cur_meetings[0].end <= meeting.start:
+            heappop(cur_meetings)
+        heappush(cur_meetings, meeting)
+        max_rooms = max(max_rooms, len(cur_meetings))
+
+    return max_rooms
+
+
 class Meeting:
-  def __init__(self, start, end):
-    self.start = start
-    self.end = end
-  
-  def __lt__(self, other):
-    return self.end < other.end
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __lt__(self, other):
+        return self.end < other.end
+
+    def __str__(self):
+        return str([self.start, self.end])
+
 
 def min_meeting_rooms(meetings):
-  # TODO: Write your code here
-  meetings.sort(key=lambda x: x.start)
-  conflict=[]
-  min_rooms=0
-  for meeting in meetings:
-    while len(conflict) > 0 and meeting.start >= conflict[0].end:
-      heappop(conflict)
-    heappush(conflict, meeting)
-    min_rooms=max(len(conflict),min_rooms)
-  return min_rooms
+    # TODO: Write your code here
+    meetings.sort(key=lambda x: x.start)
+    conflict = []
+    min_rooms = 0
+    for meeting in meetings:
+        while len(conflict) > 0 and meeting.start >= conflict[0].end:
+            heappop(conflict)
+        heappush(conflict, meeting)
+        min_rooms = max(len(conflict), min_rooms)
+    return min_rooms
 
 
 def main():
-  print("Minimum meeting rooms required: " + str(min_meeting_rooms(
-    [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
-  print("Minimum meeting rooms required: " +
-        str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)])))
-  print("Minimum meeting rooms required: " +
-        str(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)])))
-  print("Minimum meeting rooms required: " +
-        str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)])))
-  print("Minimum meeting rooms required: " + str(min_meeting_rooms(
-    [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
+    print(
+        "Minimum meeting rooms required: "
+        + str(ans([Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(ans([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(ans([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(ans([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(ans([Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)]))
+    )
+
+    print("\n")
 
 
 main()
 
 
-
-#answer
-from heapq import *
-
-
 class Meeting:
-  def __init__(self, start, end):
-    self.start = start
-    self.end = end
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
 
-  def __lt__(self, other):
-    # min heap based on meeting.end
-    return self.end < other.end
+    def __lt__(self, other):
+        # min heap based on meeting.end
+        return self.end < other.end
 
 
 def min_meeting_rooms(meetings):
-  # sort the meetings by start time
-  meetings.sort(key=lambda x: x.start)
+    # sort the meetings by start time
+    meetings.sort(key=lambda x: x.start)
 
-  minRooms = 0
-  minHeap = []
-  for meeting in meetings:
-    # remove all the meetings that have ended
-    while(len(minHeap) > 0 and meeting.start >= minHeap[0].end):
-      heappop(minHeap)
-    # add the current meeting into min_heap
-    heappush(minHeap, meeting)
-    # all active meetings are in the min_heap, so we need rooms for all of them.
-    minRooms = max(minRooms, len(minHeap))
-  return minRooms
+    minRooms = 0
+    minHeap = []
+    for meeting in meetings:
+        # remove all the meetings that have ended
+        while len(minHeap) > 0 and meeting.start >= minHeap[0].end:
+            heappop(minHeap)
+        # add the current meeting into min_heap
+        heappush(minHeap, meeting)
+        # all active meetings are in the min_heap, so we need rooms for all of them.
+        minRooms = max(minRooms, len(minHeap))
+    return minRooms
 
 
 def main():
-  print("Minimum meeting rooms required: " + str(min_meeting_rooms(
-    [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
-  print("Minimum meeting rooms required: " +
-        str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)])))
-  print("Minimum meeting rooms required: " +
-        str(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)])))
-  print("Minimum meeting rooms required: " +
-        str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)])))
-  print("Minimum meeting rooms required: " + str(min_meeting_rooms(
-    [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)])))
+    print(
+        "Minimum meeting rooms required: "
+        + str(
+            min_meeting_rooms(
+                [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)]
+            )
+        )
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(
+            min_meeting_rooms(
+                [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)]
+            )
+        )
+    )
 
 
 main()
 
 
-
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(N*logN), where ‘N’ is the total number of meetings. T
 his is due to the sorting that we did in the beginning. 
@@ -136,4 +175,4 @@ Space complexity
 The space complexity of the above algorithm will be O(N) which is required for sorting. 
 Also, in the worst case scenario, we’ll have to insert all the meetings into the Min Heap (when all meetings overlap) which will also take O(N) space. 
 The overall space complexity of our algorithm is O(N).
-'''
+"""
