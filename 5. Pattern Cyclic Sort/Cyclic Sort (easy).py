42ddb5adfb8e3ac80d845fdff05671d883ac8a62
josephhlwang <joseph001126@gmail.com>
2022-02-15 23:34:32 -0500

    Implemented solution.

--- 5. Pattern Cyclic Sort/Cyclic Sort (easy).py
+++ 5. Pattern Cyclic Sort/Cyclic Sort (easy).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. 
 This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
@@ -20,43 +20,65 @@ Example 3:
 
 Input: [1, 5, 6, 4, 3, 2]
 Output: [1, 2, 3, 4, 5, 6]
-'''
+"""
 
-#mycode
+
+def ans(nums):
+
+    # index of iterator
+    i = 0
+
+    while i < len(nums):
+        # correct index of iterator
+        j = nums[i] - 1
+
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+
+    return nums
+
+
+# mycode
 def cyclic_sort(nums):
-  # TODO: Write your code here
-  i=0
-  while i < len(nums):
-    j=nums[i]-1
-    if i != j:
-      nums[i], nums[j] = nums[j], nums[i]
-    else:
-      i += 1
-  return nums
+    # TODO: Write your code here
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if i != j:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    return nums
 
 
-#answer
+# answer
 def cyclic_sort(nums):
-  i = 0
-  while i < len(nums):
-    j = nums[i] - 1
-    if nums[i] != nums[j]:
-      nums[i], nums[j] = nums[j], nums[i]  # swap
-    else:
-      i += 1
-  return nums
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+    return nums
 
 
 def main():
-  print(cyclic_sort([3, 1, 5, 4, 2]))
-  print(cyclic_sort([2, 6, 4, 3, 1, 5]))
-  print(cyclic_sort([1, 5, 6, 4, 3, 2]))
+    print(ans([3, 1, 5, 4, 2]))
+    print(ans([2, 6, 4, 3, 1, 5]))
+    print(ans([1, 5, 6, 4, 3, 2]))
+
+    print(cyclic_sort([3, 1, 5, 4, 2]))
+    print(cyclic_sort([2, 6, 4, 3, 1, 5]))
+    print(cyclic_sort([1, 5, 6, 4, 3, 2]))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(n). 
 Although we are not incrementing the index i when swapping the numbers, this will result in more than ‘n’ iterations of the loop, 
@@ -65,4 +87,4 @@ we will move on to the next number by incrementing i. So overall, our algorithm 
 
 Space complexity 
 The algorithm runs in constant space O(1).
-'''
+"""
--- 5. Pattern Cyclic Sort/Find all Duplicate Numbers (easy).py
+++ 5. Pattern Cyclic Sort/Find all Duplicate Numbers (easy).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’. 
 The array has some duplicates, find all the duplicate numbers without using any extra space.
@@ -12,53 +12,78 @@ Example 2:
 
 Input: [5, 4, 7, 2, 3, 5, 3]
 Output: [3, 5]
-'''
+"""
 
 
-#mycode
+def ans(nums):
+
+    # index of iterator
+    i = 0
+
+    while i < len(nums):
+        # correct index of iterator
+        j = nums[i] - 1
+
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+
+    dups = []
+
+    for x in range(len(nums)):
+        if x != nums[x] - 1:
+            dups.append(nums[x])
+
+    return dups
+
+
+# mycode
 def find_all_duplicates(nums):
-  duplicateNumbers = []
-  # TODO: Write your code here
-  for i in range(len(nums)):
-    j=abs(nums[i])-1
-    if nums[j] > 0:
-      nums[j]=-nums[j]
-    else:
-      duplicateNumbers.append(j+1)
-  return duplicateNumbers
+    duplicateNumbers = []
+    # TODO: Write your code here
+    for i in range(len(nums)):
+        j = abs(nums[i]) - 1
+        if nums[j] > 0:
+            nums[j] = -nums[j]
+        else:
+            duplicateNumbers.append(j + 1)
+    return duplicateNumbers
 
 
-
-#answer
+# answer
 def find_all_duplicates(nums):
-  i = 0
-  while i < len(nums):
-    j = nums[i] - 1
-    if nums[i] != nums[j]:
-      nums[i], nums[j] = nums[j], nums[i]  # swap
-    else:
-      i += 1
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
 
-  duplicateNumbers = []
-  for i in range(len(nums)):
-    if nums[i] != i + 1:
-      duplicateNumbers.append(nums[i])
+    duplicateNumbers = []
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            duplicateNumbers.append(nums[i])
 
-  return duplicateNumbers
+    return duplicateNumbers
 
 
 def main():
-  print(find_all_duplicates([3, 4, 4, 5, 5]))
-  print(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]))
+    print(ans([3, 4, 4, 5, 5]))
+    print(ans([5, 4, 7, 2, 3, 5, 3]))
+
+    print(find_all_duplicates([3, 4, 4, 5, 5]))
+    print(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(n).
 
 Space complexity 
 Ignoring the space required for storing the duplicates, the algorithm runs in constant space O(1).
-'''
+"""
