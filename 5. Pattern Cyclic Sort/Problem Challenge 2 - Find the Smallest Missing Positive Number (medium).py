5b40e90bed0897b0836d58c5ef32894966fd453a
josephhlwang <joseph001126@gmail.com>
2022-02-18 12:55:47 -0500

    Implemented solution.

--- 5. Pattern Cyclic Sort/Problem Challenge 2 - Find the Smallest Missing Positive Number (medium).py
+++ 5. Pattern Cyclic Sort/Problem Challenge 2 - Find the Smallest Missing Positive Number (medium).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Challenge 2
 Find the Smallest Missing Positive Number (medium)
 
@@ -19,57 +19,81 @@ Example 3:
 
 Input: [3, 2, 5, 1]
 Output: 4
-'''
+"""
 
-#mycode
+
+def ans(nums):
+
+    # index of iterator
+    i = 0
+
+    while i < len(nums):
+        # correct index of iterator
+        j = nums[i] - 1
+
+        if 0 <= j < len(nums) and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+
+    for x in range(len(nums)):
+        if nums[x] != x + 1:
+            return x + 1
+
+
+# mycode
 def find_first_missing_positive(nums):
-  # TODO: Write your code here
-  i=0
-  while i < len(nums):
-    j=nums[i]-1
-    if j >= 0 and j < len(nums):
-      if nums[i] != nums[j]:
-        nums[i], nums[j] = nums[j], nums[i]
-      else:
-        i+=1
-    else:
-      i+=1
-    
-  for i in range(len(nums)):
-    if nums[i]-1 != i:
-      return i+1
+    # TODO: Write your code here
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if j >= 0 and j < len(nums):
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        else:
+            i += 1
+
+    for i in range(len(nums)):
+        if nums[i] - 1 != i:
+            return i + 1
 
 
-#answer
+# answer
 def find_first_missing_positive(nums):
-  i, n = 0, len(nums)
-  while i < n:
-    j = nums[i] - 1
-    if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
-      nums[i], nums[j] = nums[j], nums[i]  # swap
-    else:
-      i += 1
+    i, n = 0, len(nums)
+    while i < n:
+        j = nums[i] - 1
+        if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
 
-  for i in range(n):
-    if nums[i] != i + 1:
-      return i + 1
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
 
-  return nums.length + 1
+    return nums.length + 1
 
 
 def main():
-  print(find_first_missing_positive([-3, 1, 5, 4, 2]))
-  print(find_first_missing_positive([3, -2, 0, 1, 2]))
-  print(find_first_missing_positive([3, 2, 5, 1]))
+    print(ans([-3, 1, 5, 4, 2]))
+    print(ans([3, -2, 0, 1, 2]))
+    print(ans([3, 2, 5, 1]))
+
+    print(find_first_missing_positive([-3, 1, 5, 4, 2]))
+    print(find_first_missing_positive([3, -2, 0, 1, 2]))
+    print(find_first_missing_positive([3, 2, 5, 1]))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(n).
 
 Space complexity 
 The algorithm runs in constant space O(1).
-'''
+"""
