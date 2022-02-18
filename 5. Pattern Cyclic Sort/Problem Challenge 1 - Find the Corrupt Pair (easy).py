1a9976f8d04454d37df7b000bcfaee95c66a6238
josephhlwang <joseph001126@gmail.com>
2022-02-18 01:27:17 -0500

    Implemented solution.

--- 5. Pattern Cyclic Sort/Problem Challenge 1 - Find the Corrupt Pair (easy).py
+++ 5. Pattern Cyclic Sort/Problem Challenge 1 - Find the Corrupt Pair (easy).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Challenge 1
 
 Find the Corrupt Pair (easy)
@@ -16,61 +16,82 @@ Example 2:
 Input: [3, 1, 2, 3, 6, 4]
 Output: [3, 5]
 Explanation: '3' is duplicated and '5' is missing.
-'''
+"""
 
 
-#mycode
+def ans(nums):
+
+    # index of iterator
+    i = 0
+
+    while i < len(nums):
+        # correct index of iterator
+        j = nums[i] - 1
+
+        if j < len(nums) and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+
+    for x in range(len(nums)):
+        if nums[x] != x + 1:
+            return [nums[x], x + 1]
+
+
+# mycode
 def find_corrupt_numbers(nums):
-  # TODO: Write your code here
-  duplicate, missing = 0, 0
-  i=0
+    # TODO: Write your code here
+    duplicate, missing = 0, 0
+    i = 0
 
-  while i < len(nums):
-    j=nums[i]-1
-    if i != j:
-      if nums[i] != nums[j]:
-        nums[i], nums[j] = nums[j], nums[i]
-      else:
-        duplicate=nums[i]
-        i += 1
-    else:
-      i += 1
-    
-  for i in range(len(nums)):
-    if nums[i]-1 != i:
-      missing= i+1
-  return [duplicate, missing]
+    while i < len(nums):
+        j = nums[i] - 1
+        if i != j:
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                duplicate = nums[i]
+                i += 1
+        else:
+            i += 1
+
+    for i in range(len(nums)):
+        if nums[i] - 1 != i:
+            missing = i + 1
+    return [duplicate, missing]
 
 
-#answer
+# answer
 def find_corrupt_numbers(nums):
-  i = 0
-  while i < len(nums):
-    j = nums[i] - 1
-    if nums[i] != nums[j]:
-      nums[i], nums[j] = nums[j], nums[i]  # swap
-    else:
-      i += 1
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
 
-  for i in range(len(nums)):
-    if nums[i] != i + 1:
-      return [nums[i], i + 1]
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            return [nums[i], i + 1]
 
-  return [-1, -1]
+    return [-1, -1]
 
 
 def main():
-  print(find_corrupt_numbers([3, 1, 2, 5, 2]))
-  print(find_corrupt_numbers([3, 1, 2, 3, 6, 4]))
+    print(ans([3, 1, 2, 5, 2]))
+    print(ans([3, 1, 2, 3, 6, 4]))
+    print(find_corrupt_numbers([3, 1, 2, 5, 2]))
+    print(find_corrupt_numbers([3, 1, 2, 3, 6, 4]))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(n).
 
 Space complexity 
 The algorithm runs in constant space O(1).
-'''
+"""
