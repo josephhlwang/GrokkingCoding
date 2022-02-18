2774a01a843e3b6ce9584cf546c3078fbcf83419
josephhlwang <joseph001126@gmail.com>
2022-02-18 00:09:46 -0500

    Implemented solution.

--- 5. Pattern Cyclic Sort/Find the Duplicate Number (easy).py
+++ 5. Pattern Cyclic Sort/Find the Duplicate Number (easy).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Statement 
 We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. 
 The array has only one duplicate but it can be repeated multiple times. 
@@ -18,75 +18,92 @@ Example 3:
 
 Input: [2, 4, 1, 4, 4]
 Output: 4
-'''
+"""
 
-#mycode 
+
+def ans(nums):
+    i = 0
+
+    while i < len(nums):
+        j = nums[i] - 1
+
+        if j != i and nums[j] != nums[i]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+
+    for x in range(len(nums)):
+        if x != nums[x] - 1:
+            return nums[x]
+
 
 def find_duplicate(nums):
-  # TODO: Write your code here
-  for i in range(len(nums)):
-    j=abs(nums[i])-1
-    if nums[j] > 0:
-      nums[j] = -nums[j]
-    else:
-      return j+1
-  return -1
+    # TODO: Write your code here
+    for i in range(len(nums)):
+        j = abs(nums[i]) - 1
+        if nums[j] > 0:
+            nums[j] = -nums[j]
+        else:
+            return j + 1
+    return -1
 
 
-#mycode2
+# mycode2
+
 
 def find_duplicate(nums):
-  # TODO: Write your code here
-  i=0
-  while i < len(nums):
-    j=nums[i]-1
- 
-    if nums[i] != nums[j]:
-      nums[i], nums[j] = nums[j], nums[i]
-    elif nums[i] == nums[j] and i!=j:
-      return nums[i]
-    else:
-      i += 1
+    # TODO: Write your code here
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
 
-  return -1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        elif nums[i] == nums[j] and i != j:
+            return nums[i]
+        else:
+            i += 1
+
+    return -1
 
 
-#answer
+# answer
+
 
 def find_duplicate(nums):
-  i = 0
-  while i < len(nums):
-    if nums[i] != i + 1:
-      j = nums[i] - 1
-      if nums[i] != nums[j]:
-        nums[i], nums[j] = nums[j], nums[i]  # swap
-      else:  # we have found the duplicate
-        return nums[i]
-    else:
-      i += 1
+    i = 0
+    while i < len(nums):
+        if nums[i] != i + 1:
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]  # swap
+            else:  # we have found the duplicate
+                return nums[i]
+        else:
+            i += 1
 
-  return -1
+    return -1
 
 
 def main():
-  print(find_duplicate([1, 4, 4, 3, 2]))
-  print(find_duplicate([2, 1, 3, 3, 5, 4]))
-  print(find_duplicate([2, 4, 1, 4, 4]))
+    print(ans([1, 4, 4, 3, 2]))
+    print(ans([2, 1, 3, 3, 5, 4]))
+    print(find_duplicate([2, 4, 1, 4, 4]))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(n).
 
 Space complexity 
 The algorithm runs in constant space O(1) but modifies the input array.
-'''
+"""
 
 
-'''
+"""
 Similar Problems 
 Problem 1: Can we solve the above problem in O(1) space and without modifying the input array?
 
@@ -94,43 +111,44 @@ Solution: While doing the cyclic sort, we realized that the array will have a cy
 This means that we can use the fast & the slow pointer method to find the duplicate number or the start of the cycle similar to Start of LinkedList Cycle.
 
 The time complexity of the above algorithm is O(n) and the space complexity is O(1).
-'''
+"""
+
 
 def find_duplicate(arr):
-  slow, fast = arr[0], arr[arr[0]]
-  while slow != fast:
-    slow = arr[slow]
-    fast = arr[arr[fast]]
+    slow, fast = arr[0], arr[arr[0]]
+    while slow != fast:
+        slow = arr[slow]
+        fast = arr[arr[fast]]
 
-  # find cycle length
-  current = arr[arr[slow]]
-  cycleLength = 1
-  while current != arr[slow]:
-    current = arr[current]
-    cycleLength += 1
+    # find cycle length
+    current = arr[arr[slow]]
+    cycleLength = 1
+    while current != arr[slow]:
+        current = arr[current]
+        cycleLength += 1
 
-  return find_start(arr, cycleLength)
+    return find_start(arr, cycleLength)
 
 
 def find_start(arr, cycleLength):
-  pointer1, pointer2 = arr[0], arr[0]
-  # move pointer2 ahead 'cycleLength' steps
-  while cycleLength > 0:
-    pointer2 = arr[pointer2]
-    cycleLength -= 1
+    pointer1, pointer2 = arr[0], arr[0]
+    # move pointer2 ahead 'cycleLength' steps
+    while cycleLength > 0:
+        pointer2 = arr[pointer2]
+        cycleLength -= 1
 
-  # increment both pointers until they meet at the start of the cycle
-  while pointer1 != pointer2:
-    pointer1 = arr[pointer1]
-    pointer2 = arr[pointer2]
+    # increment both pointers until they meet at the start of the cycle
+    while pointer1 != pointer2:
+        pointer1 = arr[pointer1]
+        pointer2 = arr[pointer2]
 
-  return pointer1
+    return pointer1
 
 
 def main():
-  print(find_duplicate([1, 4, 4, 3, 2]))
-  print(find_duplicate([2, 1, 3, 3, 5, 4]))
-  print(find_duplicate([2, 4, 1, 4, 4]))
+    print(find_duplicate([1, 4, 4, 3, 2]))
+    print(find_duplicate([2, 1, 3, 3, 5, 4]))
+    print(find_duplicate([2, 4, 1, 4, 4]))
 
 
 main()
