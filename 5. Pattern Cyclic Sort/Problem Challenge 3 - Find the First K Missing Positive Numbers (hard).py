f46e0fb248bf9896b33da9714b50a6640774261e
josephhlwang <joseph001126@gmail.com>
2022-02-22 00:26:07 -0500

    Implemented solution.

--- 5. Pattern Cyclic Sort/Problem Challenge 3 - Find the First K Missing Positive Numbers (hard).py
+++ 5. Pattern Cyclic Sort/Problem Challenge 3 - Find the First K Missing Positive Numbers (hard).py
@@ -1,4 +1,4 @@
-'''
+"""
 Problem Challenge 3
 
 Find the First K Missing Positive Numbers (hard)
@@ -22,85 +22,116 @@ Example 3:
 Input: [-2, -3, 4], k=2
 Output: [1, 2]
 Explanation: The smallest missing positive numbers are 1 and 2.
-'''
+"""
 
-#mycode
+
+def ans(nums, k):
+
+    # index of iterator
+    i = 0
+
+    while i < len(nums):
+        # correct index of iterator
+        j = nums[i] - 1
+
+        if 0 <= j < len(nums) and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    missing = []
+    for x in range(len(nums)):
+        if nums[k] > 0 and nums[x] != x + 1:
+            missing.append(x + 1)
+        if len(missing) == k:
+            return missing
+
+
+# mycode
 def find_first_k_missing_positive(nums, k):
-  missingNumbers = []
-  # TODO: Write your code here
-  i=0
-  while i < len(nums):
-    j=nums[i]-1
-    if j >= 0 and j<len(nums) and nums[i] != nums[j]:
-      nums[i], nums[j]=nums[j], nums[i]
-    else:
-      i += 1
-  
-  i=0
-  while i < len(nums) :
-    if nums[i]-1 != i:
-      missingNumbers.append(i+1)
-    i+=1
-  
-  i=1
-  if k <= len(missingNumbers):
-    missingNumbers = missingNumbers[:k]
-  else:
-    while len(missingNumbers) < k:
-      if max(missingNumbers)+1 < max(nums) and max(missingNumbers)+1 not in nums:
-        missingNumbers.append(max(missingNumbers)+1)
-      else:
-        missingNumbers.append(max(nums)+i)
+    missingNumbers = []
+    # TODO: Write your code here
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if j >= 0 and j < len(nums) and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+
+    i = 0
+    while i < len(nums):
+        if nums[i] - 1 != i:
+            missingNumbers.append(i + 1)
         i += 1
 
-  return missingNumbers
+    i = 1
+    if k <= len(missingNumbers):
+        missingNumbers = missingNumbers[:k]
+    else:
+        while len(missingNumbers) < k:
+            if (
+                max(missingNumbers) + 1 < max(nums)
+                and max(missingNumbers) + 1 not in nums
+            ):
+                missingNumbers.append(max(missingNumbers) + 1)
+            else:
+                missingNumbers.append(max(nums) + i)
+                i += 1
+
+    return missingNumbers
 
 
-#answer
+# answer
+
 
 def find_first_k_missing_positive(nums, k):
-  n = len(nums)
-  i = 0
-  while i < len(nums):
-    j = nums[i] - 1
-    if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
-      nums[i], nums[j] = nums[j], nums[i]  # swap
-    else:
-      i += 1
+    n = len(nums)
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
 
-  missingNumbers = []
-  extraNumbers = set()
-  for i in range(n):
-    if len(missingNumbers) < k:
-      if nums[i] != i + 1:
-        missingNumbers.append(i + 1)
-        extraNumbers.add(nums[i])
+    missingNumbers = []
+    extraNumbers = set()
+    for i in range(n):
+        if len(missingNumbers) < k:
+            if nums[i] != i + 1:
+                missingNumbers.append(i + 1)
+                extraNumbers.add(nums[i])
 
-  # add the remaining missing numbers
-  i = 1
-  while len(missingNumbers) < k:
-    candidateNumber = i + n
-    # ignore if the array contains the candidate number
-    if candidateNumber not in extraNumbers:
-      missingNumbers.append(candidateNumber)
-    i += 1
+    # add the remaining missing numbers
+    i = 1
+    while len(missingNumbers) < k:
+        candidateNumber = i + n
+        # ignore if the array contains the candidate number
+        if candidateNumber not in extraNumbers:
+            missingNumbers.append(candidateNumber)
+        i += 1
 
-  return missingNumbers
+    return missingNumbers
 
 
 def main():
-  print(find_first_k_missing_positive([3, -1, 4, 5, 5], 3))
-  print(find_first_k_missing_positive([2, 3, 4], 3))
-  print(find_first_k_missing_positive([-2, -3, 4], 2))
+
+    print(find_first_k_missing_positive([3, -1, 4, 5, 5], 3))
+    print(find_first_k_missing_positive([2, 3, 4], 3))
+    print(find_first_k_missing_positive([-2, -3, 4], 2))
+
+    print(find_first_k_missing_positive([3, -1, 4, 5, 5], 3))
+    print(find_first_k_missing_positive([2, 3, 4], 3))
+    print(find_first_k_missing_positive([-2, -3, 4], 2))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(n + k), as the last two for loops will run for O(n) and O(k) times respectively.
 
 Space complexity 
 The algorithm needs O(k) space to store the extraNumbers.
-'''
+"""
