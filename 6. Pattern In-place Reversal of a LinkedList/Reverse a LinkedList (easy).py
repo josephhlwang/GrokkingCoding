05a4fe9d0f1358d3f63ac0d2688942f191504a30
josephhlwang <joseph001126@gmail.com>
2022-02-22 00:26:48 -0500

    Implemented solution.

--- 6. Pattern In-place Reversal of a LinkedList/Reverse a LinkedList (easy).py
+++ 6. Pattern In-place Reversal of a LinkedList/Reverse a LinkedList (easy).py
@@ -1,102 +1,123 @@
-'''
+"""
 Problem Statement 
 Given the head of a Singly LinkedList, reverse the LinkedList.
  Write a function to return the new head of the reversed LinkedList.
-'''
+"""
 
-#mycode
+# mycode
 from __future__ import print_function
 
 
-class Node:
-  def __init__(self, value, next=None):
-    self.value = value
-    self.next = next
+def ans(head):
+    prev = None
+    cur = head
 
-  def print_list(self):
-    temp = self
-    while temp is not None:
-      print(temp.value, end=" ")
-      temp = temp.next
-    print()
+    while cur:
+        temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+
+    return prev
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
 
 
 def reverse(head):
-  # TODO: Write your code here
-  former, latter = None, head
-  while latter:
-    temp = latter
-    latter = latter.next
-    temp.next = former
-    former = temp
-  return temp
+    # TODO: Write your code here
+    former, latter = None, head
+    while latter:
+        temp = latter
+        latter = latter.next
+        temp.next = former
+        former = temp
+    return temp
 
 
 def main():
-  head = Node(2)
-  head.next = Node(4)
-  head.next.next = Node(6)
-  head.next.next.next = Node(8)
-  head.next.next.next.next = Node(10)
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
 
-  print("Nodes of original LinkedList are: ", end='')
-  head.print_list()
-  result = reverse(head)
-  print("Nodes of reversed LinkedList are: ", end='')
-  result.print_list()
+    print("Nodes of original LinkedList are: ", end="")
+    head.print_list()
+    result = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end="")
+    result.print_list()
+
+    # head = Node(2)
+    # head.next = Node(4)
+    # head.next.next = Node(6)
+    # head.next.next.next = Node(8)
+    # head.next.next.next.next = Node(10)
+
+    print("Nodes of original LinkedList are: ", end="")
+    result.print_list()
+    result = ans(result)
+    print("Nodes of reversed LinkedList are: ", end="")
+    result.print_list()
 
 
 main()
 
 
-#answer
-from __future__ import print_function
-
-
 class Node:
-  def __init__(self, value, next=None):
-    self.value = value
-    self.next = next
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
-  def print_list(self):
-    temp = self
-    while temp is not None:
-      print(temp.value, end=" ")
-      temp = temp.next
-    print()
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
 
 
 def reverse(head):
-  previous, current, next = None, head, None
-  while current is not None:
-    next = current.next  # temporarily store the next node
-    current.next = previous  # reverse the current node
-    previous = current  # before we move to the next node, point previous to the current node
-    current = next  # move on the next node
-  return previous
+    previous, current, next = None, head, None
+    while current is not None:
+        next = current.next  # temporarily store the next node
+        current.next = previous  # reverse the current node
+        previous = current  # before we move to the next node, point previous to the current node
+        current = next  # move on the next node
+    return previous
 
 
 def main():
-  head = Node(2)
-  head.next = Node(4)
-  head.next.next = Node(6)
-  head.next.next.next = Node(8)
-  head.next.next.next.next = Node(10)
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
 
-  print("Nodes of original LinkedList are: ", end='')
-  head.print_list()
-  result = reverse(head)
-  print("Nodes of reversed LinkedList are: ", end='')
-  result.print_list()
+    print("Nodes of original LinkedList are: ", end="")
+    head.print_list()
+    result = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end="")
+    result.print_list()
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of our algorithm will be O(N) where ‘N’ is the total number of nodes in the LinkedList.
 
 Space complexity 
 We only used constant space, therefore, the space complexity of our algorithm is O(1).
-'''
+"""
