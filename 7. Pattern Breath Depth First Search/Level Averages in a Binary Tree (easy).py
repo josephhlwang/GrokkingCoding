1307b10296d9e7f9c7ebd4b08b7d52c126b07173
josephhlwang <joseph001126@gmail.com>
2022-02-23 11:20:52 -0500

    Implemented solution.

--- 7. Pattern Breath Depth First Search/Level Averages in a Binary Tree (easy).py
+++ 7. Pattern Breath Depth First Search/Level Averages in a Binary Tree (easy).py
@@ -1,107 +1,131 @@
-'''
+"""
 Problem Statement 
 Given a binary tree, populate an array to represent the averages of all of its levels.
-'''
+"""
 
-#mycode
+# mycode
 from collections import deque
 
+
+def ans(root):
+    result = []
+    queue = deque()
+    cur_sum, cur_level, cur_level_copy, next_level = 0, 1, 1, 0
+
+    queue.append(root)
+
+    while len(queue):
+        cur = queue.popleft()
+        cur_sum += cur.val
+        cur_level -= 1
+
+        if cur.left:
+            queue.append(cur.left)
+            next_level += 1
+
+        if cur.right:
+            queue.append(cur.right)
+            next_level += 1
+
+        if cur_level == 0:
+            result.append(cur_sum / cur_level_copy)
+            cur_sum = 0
+            cur_level, cur_level_copy, next_level = next_level, next_level, 0
+
+    return result
+
+
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right = None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
 
 
 def find_level_averages(root):
-  result = []
-  # TODO: Write your code here
-  if not root:
+    result = []
+    # TODO: Write your code here
+    if not root:
+        return result
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        value = 0
+        n = len(queue)
+        for i in range(n):
+            current = queue.popleft()
+            value += current.val
+
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+        result.append(value / n)
+
     return result
-  queue = deque()
-  queue.append(root)
-
-  while queue:
-    value = 0
-    n = len(queue)
-    for i in range(n):
-      current = queue.popleft()
-      value += current.val
-
-      if current.left:
-        queue.append(current.left)
-      if current.right:
-        queue.append(current.right) 
-    result.append(value/n) 
-
-  return result
 
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.left.right = TreeNode(2)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  print("Level averages are: " + str(find_level_averages(root)))
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level averages are: " + str(ans(root)))
 
 
 main()
 
 
-
-#answer
-from collections import deque
-
-
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right = None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
 
 
 def find_level_averages(root):
-  result = []
-  if root is None:
+    result = []
+    if root is None:
+        return result
+
+    queue = deque()
+    queue.append(root)
+    while queue:
+        levelSize = len(queue)
+        levelSum = 0.0
+        for _ in range(levelSize):
+            currentNode = queue.popleft()
+            # add the node's value to the running sum
+            levelSum += currentNode.val
+            # insert the children of current node to the queue
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
+
+        # append the current level's average to the result array
+        result.append(levelSum / levelSize)
+
     return result
-
-  queue = deque()
-  queue.append(root)
-  while queue:
-    levelSize = len(queue)
-    levelSum = 0.0
-    for _ in range(levelSize):
-      currentNode = queue.popleft()
-      # add the node's value to the running sum
-      levelSum += currentNode.val
-      # insert the children of current node to the queue
-      if currentNode.left:
-        queue.append(currentNode.left)
-      if currentNode.right:
-        queue.append(currentNode.right)
-
-    # append the current level's average to the result array
-    result.append(levelSum / levelSize)
-
-  return result
 
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.left.right = TreeNode(2)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  print("Level averages are: " + str(find_level_averages(root)))
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level averages are: " + str(find_level_averages(root)))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. 
 This is due to the fact that we traverse each node once.
@@ -110,4 +134,4 @@ Space complexity
 The space complexity of the above algorithm will be O(N)O which is required for the queue. 
 Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level), 
 therefore we will need O(N) space to store them in the queue.
-'''
+"""
