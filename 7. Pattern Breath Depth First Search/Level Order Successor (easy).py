c40b047357b3d2d0eab69727b4ceecf74174390b
josephhlwang <joseph001126@gmail.com>
2022-02-25 00:43:04 -0500

    Implemented solution.

--- 7. Pattern Breath Depth First Search/Level Order Successor (easy).py
+++ 7. Pattern Breath Depth First Search/Level Order Successor (easy).py
@@ -1,115 +1,134 @@
-'''
+"""
 Problem Statement 
 Given a binary tree and a node, find the level order successor of the given node in the tree. 
 The level order successor is the node that appears right after the given node in the level order traversal.
-'''
+"""
 
 
-#mycode
+# mycode
 from collections import deque
 
 
+def ans(root, key):
+    queue = deque()
+
+    queue.append(root)
+
+    while len(queue):
+        cur = queue.popleft()
+
+        if cur.left:
+            queue.append(cur.left)
+
+        if cur.right:
+            queue.append(cur.right)
+
+        if cur.val == key:
+            break
+
+    return queue[0]
+
+
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right = None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
 
 
 def find_successor(root, key):
-  # TODO: Write your code here
-  if not root:
+    # TODO: Write your code here
+    if not root:
+        return None
+
+    queue = deque()
+    queue.append(root)
+
+    flag = False
+    while queue:
+
+        for i in range(len(queue)):
+            current = queue.popleft()
+            if flag:
+                return current
+
+            if current.val == key:
+                flag = True
+
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
     return None
-  
-  queue = deque()
-  queue.append(root)
 
-  flag = False
-  while queue:
-    
-      
-    for i in range(len(queue)):
-      current = queue.popleft()
-      if flag:
-        return current
-
-      if current.val == key:
-        flag = True
-      
-      if current.left:
-        queue.append(current.left)
-      if current.right:
-        queue.append(current.right)
-  return None
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  result = find_successor(root, 12)
-  if result:
-    print(result.val)
-  result = find_successor(root, 10)
-  if result:
-    print(result.val)
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    result = ans(root, 12)
+    if result:
+        print(result.val)
+    result = ans(root, 9)
+    if result:
+        print(result.val)
 
 
 main()
 
 
-
-#answer
+# answer
 from collections import deque
 
 
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right = None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
 
 
 def find_successor(root, key):
-  if root is None:
-    return None
+    if root is None:
+        return None
 
-  queue = deque()
-  queue.append(root)
-  while queue:
-    currentNode = queue.popleft()
-    # insert the children of current node in the queue
-    if currentNode.left:
-      queue.append(currentNode.left)
-    if currentNode.right:
-      queue.append(currentNode.right)
+    queue = deque()
+    queue.append(root)
+    while queue:
+        currentNode = queue.popleft()
+        # insert the children of current node in the queue
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
 
-    # break if we have found the key
-    if currentNode.val == key:
-      break
+        # break if we have found the key
+        if currentNode.val == key:
+            break
 
-  return queue[0] if queue else None
+    return queue[0] if queue else None
 
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  result = find_successor(root, 12)
-  if result:
-    print(result.val)
-  result = find_successor(root, 9)
-  if result:
-    print(result.val)
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    result = find_successor(root, 12)
+    if result:
+        print(result.val)
+    result = find_successor(root, 9)
+    if result:
+        print(result.val)
 
 
 main()
 
 
-'''
+"""
 Time complexity
 The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. 
 This is due to the fact that we traverse each node once.
@@ -118,4 +137,4 @@ Space complexity
 The space complexity of the above algorithm will be O(N) which is required for the queue. 
 Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level), 
 therefore we will need O(N) space to store them in the queue.
-'''
+"""
