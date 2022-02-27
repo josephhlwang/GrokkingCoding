9537b5b48b9c3d2de04b7872dc672cf212086179
josephhlwang <joseph001126@gmail.com>
2022-02-27 01:24:08 -0500

    Implemented solution.

--- 7. Pattern Breath Depth First Search/Problem Challenge 2 - Right View of a Binary Tree (easy).py
+++ 7. Pattern Breath Depth First Search/Problem Challenge 2 - Right View of a Binary Tree (easy).py
@@ -1,119 +1,133 @@
-'''
+"""
 Problem Challenge 2
 
 Right View of a Binary Tree (easy)
 
 Given a binary tree, return an array containing nodes in its right view. 
 The right view of a binary tree is the set of nodes visible when the tree is seen from the right side.
-'''
+"""
 
 
-#mycode
+# mycode
 from __future__ import print_function
 from collections import deque
 
 
+def ans(root):
+    queue = deque()
+    queue.append(root)
+    result = []
+    cur_level, next_level = 1, 0
+    while len(queue):
+        cur = queue.popleft()
+        cur_level -= 1
+        if cur.left:
+            queue.append(cur.left)
+            next_level += 1
+        if cur.right:
+            queue.append(cur.right)
+            next_level += 1
+        if cur_level == 0:
+            result.append(cur)
+            cur_level, next_level = next_level, 0
+
+    return result
+
+
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right = None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
 
 
 def tree_right_view(root):
-  result = []
-  # TODO: Write your code here
-  if not root:
+    result = []
+    # TODO: Write your code here
+    if not root:
+        return result
+
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        for i in range(len(queue)):
+
+            current = queue.popleft()
+
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+
+        result.append(current)
+
     return result
 
-  queue = deque()
-  queue.append(root)
-
-  while queue:
-    for i in range(len(queue)):
-
-      current = queue.popleft()
-      
-      if current.left:
-        queue.append(current.left)
-      if current.right:
-        queue.append(current.right)
-        
-    result.append(current)
-
-  return result
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  root.left.left.left = TreeNode(3)
-  result = tree_right_view(root)
-  print("Tree right view: ")
-  for node in result:
-    print(str(node.val) + " ", end='')
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    root.left.left.left = TreeNode(3)
+    result = ans(root)
+    print("Tree right view: ", end="")
+    for node in result:
+        print(str(node.val) + " ", end="")
 
 
 main()
 
 
-
-
-#answer
-from __future__ import print_function
-from collections import deque
-
-
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right = None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
 
 
 def tree_right_view(root):
-  result = []
-  if root is None:
+    result = []
+    if root is None:
+        return result
+
+    queue = deque()
+    queue.append(root)
+    while queue:
+        levelSize = len(queue)
+        for i in range(0, levelSize):
+            currentNode = queue.popleft()
+            # if it is the last node of this level, add it to the result
+            if i == levelSize - 1:
+                result.append(currentNode)
+            # insert the children of current node in the queue
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
+
     return result
-
-  queue = deque()
-  queue.append(root)
-  while queue:
-    levelSize = len(queue)
-    for i in range(0, levelSize):
-      currentNode = queue.popleft()
-      # if it is the last node of this level, add it to the result
-      if i == levelSize - 1:
-        result.append(currentNode)
-      # insert the children of current node in the queue
-      if currentNode.left:
-        queue.append(currentNode.left)
-      if currentNode.right:
-        queue.append(currentNode.right)
-
-  return result
 
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  root.left.left.left = TreeNode(3)
-  result = tree_right_view(root)
-  print("Tree right view: ")
-  for node in result:
-    print(str(node.val) + " ", end='')
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    root.left.left.left = TreeNode(3)
+    result = tree_right_view(root)
+    print("Tree right view: ")
+    for node in result:
+        print(str(node.val) + " ", end="")
 
 
 main()
 
 
-
-'''
+"""
 Time complexity #
 The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. 
 This is due to the fact that we traverse each node once.
@@ -122,14 +136,13 @@ Space complexity
 The space complexity of the above algorithm will be O(N) as we need to return a list containing the level order traversal. 
 We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level 
 (this could happen only at the lowest level), therefore we will need O(N) space to store them in the queue.
-'''
+"""
 
-'''
+"""
 Similar Questions #
 Problem 1: Given a binary tree, return an array containing nodes in its left view. 
 The left view of a binary tree is the set of nodes visible when the tree is seen from the left side.
 
 Solution: We will be following a similar approach, 
 but instead of appending the last element of each level we will be appending the first element of each level to the output array.
-'''
-
+"""
