97e69b261cc17209a3dcfe5c0a1932766187802b
josephhlwang <joseph001126@gmail.com>
2022-02-27 01:23:54 -0500

    Implemented solution.

--- 7. Pattern Breath Depth First Search/Problem Challenge 1 - Connect All Level Order Siblings (medium).py
+++ 7. Pattern Breath Depth First Search/Problem Challenge 1 - Connect All Level Order Siblings (medium).py
@@ -1,124 +1,137 @@
-'''
+"""
 Problem Challenge 1
 
 Connect All Level Order Siblings (medium) 
 Given a binary tree, connect each node with its level order successor. 
 The last node of each level should point to the first node of the next level.
-'''
+"""
 
-#mycode
+# mycode
 from __future__ import print_function
 from collections import deque
 
 
-class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right, self.next = None, None, None
+def ans(root):
+    queue = deque()
+    queue.append(root)
 
-  # tree traversal using 'next' pointer
-  def print_tree(self):
-    print("Traversal using 'next' pointer: ", end='')
-    current = self
-    while current:
-      print(str(current.val) + " ", end='')
-      current = current.next
+    while len(queue):
+        cur = queue.popleft()
+
+        if cur.left:
+            queue.append(cur.left)
+
+        if cur.right:
+            queue.append(cur.right)
+
+        if len(queue):
+            next_node = queue[0]
+            cur.next = next_node
+
+    return root
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right, self.next = None, None, None
+
+    # tree traversal using 'next' pointer
+    def print_tree(self):
+        print("Traversal using 'next' pointer: ", end="")
+        current = self
+        while current:
+            print(str(current.val) + " ", end="")
+            current = current.next
 
 
 def connect_all_siblings(root):
-  # TODO: Write your code here
-  if not root:
-    return None
-  
-  queue = deque()
-  queue.append(root)
+    # TODO: Write your code here
+    if not root:
+        return None
 
-  previous = None
-  while queue:
-    for i in range(len(queue)):
-      current = queue.popleft()
+    queue = deque()
+    queue.append(root)
 
-      if previous is None:
-        previous = current
-      previous.next = current
-      
-      if current.left:
-        queue.append(current.left)
-      if current.right:
-        queue.append(current.right)
-      previous = current
-  
+    previous = None
+    while queue:
+        for i in range(len(queue)):
+            current = queue.popleft()
+
+            if previous is None:
+                previous = current
+            previous.next = current
+
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+            previous = current
+
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  connect_all_siblings(root)
-  root.print_tree()
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    ans(root)
+    root.print_tree()
 
 
 main()
 
 
-
-
-#answer
-from __future__ import print_function
-from collections import deque
-
-
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left, self.right, self.next = None, None, None
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right, self.next = None, None, None
 
-  # tree traversal using 'next' pointer
-  def print_tree(self):
-    print("Traversal using 'next' pointer: ", end='')
-    current = self
-    while current:
-      print(str(current.val) + " ", end='')
-      current = current.next
+    # tree traversal using 'next' pointer
+    def print_tree(self):
+        print("Traversal using 'next' pointer: ", end="")
+        current = self
+        while current:
+            print(str(current.val) + " ", end="")
+            current = current.next
 
 
 def connect_all_siblings(root):
-  if root is None:
-    return
+    if root is None:
+        return
 
-  queue = deque()
-  queue.append(root)
-  currentNode, previousNode = None, None
-  while queue:
-    currentNode = queue.popleft()
-    if previousNode:
-      previousNode.next = currentNode
-    previousNode = currentNode
+    queue = deque()
+    queue.append(root)
+    currentNode, previousNode = None, None
+    while queue:
+        currentNode = queue.popleft()
+        if previousNode:
+            previousNode.next = currentNode
+        previousNode = currentNode
 
-    # insert the children of current node in the queue
-    if currentNode.left:
-      queue.append(currentNode.left)
-    if currentNode.right:
-      queue.append(currentNode.right)
+        # insert the children of current node in the queue
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
 
 
 def main():
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(9)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  connect_all_siblings(root)
-  root.print_tree()
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_all_siblings(root)
+    root.print_tree()
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. 
 This is due to the fact that we traverse each node once.
@@ -127,5 +140,4 @@ Space complexity
 The space complexity of the above algorithm will be O(N) which is required for the queue. 
 Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level), 
 therefore we will need O(N) space to store them in the queue.
-'''
-
+"""
