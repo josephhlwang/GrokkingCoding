52e9e0c37b6be0811ada805b6f5f539cf8e221c8
josephhlwang <joseph001126@gmail.com>
2022-02-27 23:10:22 -0500

    Implemented solution.

--- 8. Pattern Tree Depth First Search/Path With Given Sequence (medium).py
+++ 8. Pattern Tree Depth First Search/Path With Given Sequence (medium).py
@@ -1,106 +1,129 @@
-'''
+"""
 Problem Statement 
 Given a binary tree and a number sequence, find if the sequence is present as a root-to-leaf path in the given tree.
-'''
+"""
 
-#mycode
+
+def ans_caller(root, sequence):
+    return ans(root, sequence, 0)
+
+
+def ans(root, sequence, index):
+    if root == None:
+        return False
+
+    if root.val == sequence[index]:
+        index += 1
+        if index == len(sequence):
+            return True
+        return ans(root.left, sequence, index) or ans(root.right, sequence, index)
+
+    return False
+
+
+# mycode
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def find_path(root, sequence):
-  # TODO: Write your code here
-  
-  return find_current_path(root, sequence)
+    # TODO: Write your code here
+
+    return find_current_path(root, sequence)
+
 
 def find_current_path(currentNode, sequence):
-  
-  if not currentNode:
-    return False
 
-  if currentNode.val != sequence[0]:
-    return False
-  else:
-    if currentNode.left is None and currentNode.right is None:
-      return True
+    if not currentNode:
+        return False
+
+    if currentNode.val != sequence[0]:
+        return False
     else:
-      del sequence[0]
-      return find_current_path(currentNode.left, sequence) or find_current_path(currentNode.right, sequence)
-  
-
+        if currentNode.left is None and currentNode.right is None:
+            return True
+        else:
+            del sequence[0]
+            return find_current_path(currentNode.left, sequence) or find_current_path(
+                currentNode.right, sequence
+            )
 
 
 def main():
 
-  root = TreeNode(1)
-  root.left = TreeNode(0)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(1)
-  root.right.left = TreeNode(6)
-  root.right.right = TreeNode(5)
+    root = TreeNode(1)
+    root.left = TreeNode(0)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(1)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(5)
 
-  print("Tree has path sequence: " + str(find_path(root, [1, 0, 7])))
-  print("Tree has path sequence: " + str(find_path(root, [1, 1, 6])))
+    print("Tree has path sequence: " + str(ans_caller(root, [1, 0, 7])))
+    print("Tree has path sequence: " + str(ans_caller(root, [1, 1, 6])))
 
 
 main()
 
 
-
-#answer
+# answer
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def find_path(root, sequence):
-  if not root:
-    return len(sequence) == 0
+    if not root:
+        return len(sequence) == 0
 
-  return find_path_recursive(root, sequence, 0)
+    return find_path_recursive(root, sequence, 0)
 
 
 def find_path_recursive(currentNode, sequence, sequenceIndex):
 
-  if currentNode is None:
-    return False
+    if currentNode is None:
+        return False
 
-  seqLen = len(sequence)
-  if sequenceIndex >= seqLen or currentNode.val != sequence[sequenceIndex]:
-    return False
+    seqLen = len(sequence)
+    if sequenceIndex >= seqLen or currentNode.val != sequence[sequenceIndex]:
+        return False
 
-  # if the current node is a leaf, add it is the end of the sequence, we have found a path!
-  if currentNode.left is None and currentNode.right is None and sequenceIndex == seqLen - 1:
-    return True
+    # if the current node is a leaf, add it is the end of the sequence, we have found a path!
+    if (
+        currentNode.left is None
+        and currentNode.right is None
+        and sequenceIndex == seqLen - 1
+    ):
+        return True
 
-  # recursively call to traverse the left and right sub-tree
-  # return true if any of the two recursive call return true
-  return find_path_recursive(currentNode.left, sequence, sequenceIndex + 1) or \
-         find_path_recursive(currentNode.right, sequence, sequenceIndex + 1)
+    # recursively call to traverse the left and right sub-tree
+    # return true if any of the two recursive call return true
+    return find_path_recursive(
+        currentNode.left, sequence, sequenceIndex + 1
+    ) or find_path_recursive(currentNode.right, sequence, sequenceIndex + 1)
 
 
 def main():
 
-  root = TreeNode(1)
-  root.left = TreeNode(0)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(1)
-  root.right.left = TreeNode(6)
-  root.right.right = TreeNode(5)
+    root = TreeNode(1)
+    root.left = TreeNode(0)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(1)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(5)
 
-  print("Tree has path sequence: " + str(find_path(root, [1, 0, 7])))
-  print("Tree has path sequence: " + str(find_path(root, [1, 1, 6])))
+    print("Tree has path sequence: " + str(find_path(root, [1, 0, 7])))
+    print("Tree has path sequence: " + str(find_path(root, [1, 1, 6])))
 
 
 main()
 
 
-'''
+"""
 Time complexity 
 The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. 
 This is due to the fact that we traverse each node once.
@@ -109,4 +132,4 @@ Space complexity
 The space complexity of the above algorithm will be O(N) in the worst case. 
 This space will be used to store the recursion stack. 
 The worst case will happen when the given tree is a linked list (i.e., every node has only one child).
-'''
+"""
