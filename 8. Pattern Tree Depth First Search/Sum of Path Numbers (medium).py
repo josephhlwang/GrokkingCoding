59b8e7cbdad1556e27c0d31ec1e93ba31e86010e
josephhlwang <joseph001126@gmail.com>
2022-03-02 00:59:43 -0500

    Implemented solution.

--- 8. Pattern Tree Depth First Search/Sum of Path Numbers (medium).py
+++ 8. Pattern Tree Depth First Search/Sum of Path Numbers (medium).py
@@ -1,90 +1,110 @@
-'''
+"""
 Problem Statement 
 Given a binary tree where each node can only have a digit (0-9) value, 
 each root-to-leaf path will represent a number. Find the total sum of all the numbers represented by all paths.
-'''
+"""
 
-#mycode
+
+def ans(root, sum):
+
+    if root == None:
+        return 0
+
+    path_sum = sum * 10 + root.val
+
+    if root.left == None and root.right == None:
+        return path_sum
+
+    return ans(root.left, path_sum) + ans(root.right, path_sum)
+
+
+def ans_caller(root):
+    return ans(root, 0)
+
+
+# mycode
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def find_sum_of_path_numbers(root):
-  # TODO: Write your code here
-  return find_root_to_leaf_path_numbers(root, 0)
+    # TODO: Write your code here
+    return find_root_to_leaf_path_numbers(root, 0)
+
 
 def find_root_to_leaf_path_numbers(currentNode, currentSum):
-  if not currentNode:
-    return 0
-  
-  currentSum = currentSum*10 + currentNode.val
+    if not currentNode:
+        return 0
 
-  if currentNode.left is None and currentNode.right is None:
-    return currentSum
-  else:
-    return find_root_to_leaf_path_numbers(currentNode.left, currentSum) + find_root_to_leaf_path_numbers(currentNode.right, currentSum)
+    currentSum = currentSum * 10 + currentNode.val
 
-
+    if currentNode.left is None and currentNode.right is None:
+        return currentSum
+    else:
+        return find_root_to_leaf_path_numbers(
+            currentNode.left, currentSum
+        ) + find_root_to_leaf_path_numbers(currentNode.right, currentSum)
 
 
 def main():
-  root = TreeNode(1)
-  root.left = TreeNode(0)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(1)
-  root.right.left = TreeNode(6)
-  root.right.right = TreeNode(5)
-  print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
+    root = TreeNode(1)
+    root.left = TreeNode(0)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(1)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(5)
+    print("Total Sum of Path Numbers: " + str(ans_caller(root)))
 
 
 main()
 
 
-
-#answer
+# answer
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def find_sum_of_path_numbers(root):
-  return find_root_to_leaf_path_numbers(root, 0)
+    return find_root_to_leaf_path_numbers(root, 0)
 
 
 def find_root_to_leaf_path_numbers(currentNode, pathSum):
-  if currentNode is None:
-    return 0
+    if currentNode is None:
+        return 0
 
-  # calculate the path number of the current node
-  pathSum = 10 * pathSum + currentNode.val
+    # calculate the path number of the current node
+    pathSum = 10 * pathSum + currentNode.val
 
-  # if the current node is a leaf, return the current path sum
-  if currentNode.left is None and currentNode.right is None:
-    return pathSum
+    # if the current node is a leaf, return the current path sum
+    if currentNode.left is None and currentNode.right is None:
+        return pathSum
 
-  # traverse the left and the right sub-tree
-  return find_root_to_leaf_path_numbers(currentNode.left, pathSum) + find_root_to_leaf_path_numbers(currentNode.right, pathSum)
+    # traverse the left and the right sub-tree
+    return find_root_to_leaf_path_numbers(
+        currentNode.left, pathSum
+    ) + find_root_to_leaf_path_numbers(currentNode.right, pathSum)
 
 
 def main():
-  root = TreeNode(1)
-  root.left = TreeNode(0)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(1)
-  root.right.left = TreeNode(6)
-  root.right.right = TreeNode(5)
-  print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
+    root = TreeNode(1)
+    root.left = TreeNode(0)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(1)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(5)
+    print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
 
 
 main()
 
 
-'''
+"""
 Time complexity #
 The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. 
 This is due to the fact that we traverse each node once.
@@ -93,4 +113,4 @@ Space complexity #
 The space complexity of the above algorithm will be O(N) in the worst case. 
 This space will be used to store the recursion stack. 
 The worst case will happen when the given tree is a linked list (i.e., every node has only one child).
-'''
+"""
